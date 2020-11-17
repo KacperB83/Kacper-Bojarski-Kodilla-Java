7e75907ab73d8d1a4d000bf990de95508574af4d
Kacper Bojarski <k.bojarski.pl@gmail.com>
2020-11-17 18:49:18 +0100

    6.3. JUnit, czyli pierwsze testy!

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -9,15 +9,25 @@ import java.util.List;
 
 @DisplayName("List test ")
 class CollectionTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterAll
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
     @BeforeEach
-    public void before() {
-        System.out.println("Test case: begin");
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test # " + testCounter);
     }
-    @AfterEach
-    public void after() {
-        System.out.println("Test case: end");
-    }
-    @DisplayName("when create List with oddNumbers, " +
+
+    @DisplayName("when create List with null , " +
             "then check if it's empty"
     )
     @Test
@@ -25,17 +35,26 @@ class CollectionTestSuite {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         List<Integer> numbers = new ArrayList<>();
-
-        numbers.add(null);
-        numbers.add(0);
         //When
+        numbers.add(null);
         System.out.println("Checking if can put null values");
-
         //Then
-        //Assertions.assertFalse();
+        Assertions.assertEquals(null, numbers.get(0));
 
     }
+    @DisplayName("when create List with odd value , " +
+            "then check if it was put in List "
+    )
+    @Test
     void testOddNumbersExterminatorNormalList () {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        List<Integer> numbers = new ArrayList<>();
+        //When
+        numbers.add(6);
 
+        System.out.println("Checking if can put odd value");
+        //Then
+        Assertions.assertEquals(6, numbers.get(0));
     }
 }
