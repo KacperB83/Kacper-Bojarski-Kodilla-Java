6cd6131f4a8d3baa2164e4a3b2a16c6d0263aa49
Kacper Bojarski <k.bojarski.pl@gmail.com>
2020-11-27 11:59:43 +0100

    7.2 Zadanie: niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -8,8 +9,11 @@ import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        /*PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("Text", (text -> "ABC "+text+" ABC"));
         poemBeautifier.beautify("Text", (text -> text.toUpperCase()));
         poemBeautifier.beautify("Text", (text -> "####--- "+text+" ---####"));
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -6,8 +6,5 @@ public class PoemBeautifier {
         String decoratedText = poemDecorator.decorate(text);
         System.out.println("Decorated text: " + decoratedText);
     }
-    @Override
-    public String toString(){
-        return this.toString();
-    }
+
 }
